12ccf5fe923e44be827f221b7584daf1e70d3563
sananabbasov <sanan_abbasov@list.ru>
2022-01-26 17:00:41 +0400

    26.01.2022 17:00

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,4 +1,4 @@
-Create table Singers (
+﻿Create table Singers (
 	ID int Identity(1,1) Primary KEy,
 	SingerName nvarchar(100),
 	SingerSurname nvarchar(100),
@@ -60,3 +60,63 @@ create table LikedList(
 	UserID int FOREIGN KEY REFERENCES Users(ID),
 	SongID int FOREIGN KEY REFERENCES Songs(ID),
 )
+
+
+
+insert into Singers (SingerName,SingerSurname,Birthday,PhotoUrl)
+values 
+('Röya', N'Ayxan','06.14.1982','sekil2.jpg'),
+('Elza ', N'Seyidcahan','07.07.1968','sekil3.jpg')
+
+
+insert into Genres(GenreName)
+values
+('Rap'),('Caz'),('Pop'),('Hip-hop')
+
+insert into Songs (SongName,Duration,LaunchDate)
+values 
+(N'Zəng elə',120,'01.01.2021'),
+(N'Karma ',180,'05.05.2021'),
+(N'Kəpənəklər  ',180,'07.07.2021'),
+(N'Bu Gecə',180,'07.07.2021'),
+(N'Ulduz nağılı',180,'07.07.2021'),
+(N'Dostumsan yoxsa sevgilim',180,'07.07.2021'),
+(N'Gözəllik',180,'07.07.2021')
+
+
+insert into SingerToSong (SingerID,SongID)
+values (2,1),(2,2),(2,3),(6,4),(5,5),(5,6),(6,7)
+
+insert into SongToGenre(SongID,GenreID)
+values (1,1),(2,2),(3,3),(4,3),(5,3),(6,3),(7,3)
+
+
+
+Select Singers.SingerName, Singers.SingerSurname
+from Singers 
+INNER JOIN SingerToSong on Singers.ID = SingerToSong.SingerID
+
+
+select * from Songs
+
+
+select Singers.SingerName, Singers.SingerSurname,
+Songs.SongName, Songs.Duration, Songs.LaunchDate
+from SingerToSong
+inner join Singers on SingerID = Singers.ID
+inner join Songs on SongID = Songs.ID
+
+
+
+create view [MySongList] as
+select Singers.SingerName, Singers.SingerSurname,
+Songs.SongName, Songs.Duration, Songs.LaunchDate
+from SingerToSong
+inner join Singers on SingerID = Singers.ID
+inner join Songs on SongID = Songs.ID
+
+
+
+
+
+Select * from [MySongList]
